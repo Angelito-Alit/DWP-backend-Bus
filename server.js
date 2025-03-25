f654aa84cfaf408449868dcbe22bb439d5bd57f4
Ángel López Martínez <angelserilopezmartinez@gmail.com>
2025-03-25 17:14:30 -0600

    correcion 6

--- server.js
+++ server.js
@@ -23,16 +23,10 @@ const sse = new SSE([], {
   isSerialized: true 
 });
 
-const allowedOrigins = JSON.parse(process.env.CORS_ORIGINS);
+const allowedOrigins = process.env.CORS_ORIGIN.split(',');
 
 app.use(cors({
-  origin: function (origin, callback) {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
+  origin: allowedOrigins,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,
